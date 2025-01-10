b8d9118282dee67903d012fb782ca788d9271cca
Edvinas <edvinas2121@hotmail.com>
2025-01-10 17:21:05 +0200

    taskbar button minimize maximize toggle

--- src/screen_parts/taskbar/TaskbarBtn.tsx
+++ src/screen_parts/taskbar/TaskbarBtn.tsx
@@ -1,6 +1,6 @@
 import styles from './Taskbar.module.css';
 import { useDispatch } from "react-redux";
-import { bringWindowToFront } from '../../state/screen/screenSlice';
+import { bringWindowToFront, minimizeWindow } from '../../state/screen/screenSlice';
 import { AppDispatch } from "../../state/store";
 import icons from '../../assets/images/icons/289(32x32).png'
 
@@ -11,10 +11,14 @@ interface BtnProps {
 
 export function TaskbarBtn({name, isFocused}: BtnProps){
   const dispatch = useDispatch<AppDispatch>();
+  function onClickHandler() {
+    if (isFocused) dispatch(minimizeWindow(name));
+    else dispatch(bringWindowToFront(name));
+  }
   return (
     <div 
       className={`${styles.taskbar_button} ${isFocused ? styles.focused : styles.unfocused}`}
-      onClick={()=>dispatch(bringWindowToFront(name))}
+      onClick={onClickHandler}
     >
       <img src={icons} style={{height: '50%'}}/>
       {name}
