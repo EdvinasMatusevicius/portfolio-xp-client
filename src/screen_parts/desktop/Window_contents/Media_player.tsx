5e872a0f8d09a776b93e82d4e31ea6f992109c31
Edvinas <edvinas2121@hotmail.com>
2025-04-01 23:05:57 +0300

    on new station double click auto play

--- src/screen_parts/desktop/Window_contents/Media_player.tsx
+++ src/screen_parts/desktop/Window_contents/Media_player.tsx
@@ -45,6 +45,7 @@ export function MediaPlayer({windowIsMaximized}: {windowIsMaximized: boolean}): 
     function onTrackDoubleClick(trackIndex: number) {
         setHighlightedTrackIndex(null);
         setCurrentStation(trackIndex);
+        if (!isPlaying) setIsPlaying(true);
     }
 
     function setNewVolume(volVal: number) {
