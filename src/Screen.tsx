212020a84a4f986541d64422e0d035337d993743
Edvinas <edvinas2121@hotmail.com>
2025-04-03 22:49:51 +0300

    removed test code

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,33 +1,18 @@
-// import { Desktop } from "./screen_parts/Desktop"
-// import { Taskbar } from "./screen_parts/Taskbar"
-// import backgroundImage from './assets/images/xp_background.jpg';
-
-import { useState } from "react"
+import { Desktop } from "./screen_parts/Desktop"
+import { Taskbar } from "./screen_parts/Taskbar"
+import backgroundImage from './assets/images/xp_background.jpg';
 
 export function Screen() {
-  const [color, setColor] = useState<boolean>(true);
-  const [text, setText] = useState<string>('START');
-  function onTouchEnd() {
-    setColor(!color)
-  }
-  function onTouchStart() {
-    setText(JSON.stringify(Math.random()))
-  }
-  return <div 
-    style={{ height: '100vh', width: '100vw', backgroundColor: color ? 'green' : 'black'}}
-    onTouchStart={()=>onTouchStart()}
-    onTouchEnd={()=>onTouchEnd()}
-  ><b className="m-5 text-white">{text}</b></div>
-  // return <div style={{
-  //   width: '100vw',
-  //   height: '100vh',
-  //   backgroundImage: `url(${backgroundImage})`,
-  //   backgroundSize: 'cover',
-  //   display: 'grid',
-  //   gridTemplateRows: '1fr 35px',
-  //   overflow: 'hidden'
-  // }}>
-  //   <Desktop />
-  //   <Taskbar />
-  // </div>
+  return <div style={{
+    width: '100vw',
+    height: '100vh',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    display: 'grid',
+    gridTemplateRows: '1fr 35px',
+    overflow: 'hidden'
+  }}>
+    <Desktop />
+    <Taskbar />
+  </div>
 }
